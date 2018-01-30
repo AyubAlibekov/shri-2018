96d0a13a8dbb4feaabe162e3b7d55baf57860f22
ayub <ayub.alibekov@progforce.com>
2018-01-30 16:44:23 +0300

    requare cors

--- api-meeting-room/index.js
+++ api-meeting-room/index.js
@@ -6,11 +6,15 @@ const bodyParser = require('body-parser');
 const pagesRoutes = require('./pages/routes');
 const graphqlRoutes = require('./graphql/routes');
 
+const cors = require('cors')
+
 const app = express();
+
 
 app.use(bodyParser.json());
 
 app.use('/', pagesRoutes);
+app.use(cors());
 //graphgl instead of graphql
 app.use('/graphql', graphqlRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
